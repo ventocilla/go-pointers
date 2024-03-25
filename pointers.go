d8240d7a05d577355f8fa1d9cae28c3a004443ea
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-03-25 18:23:13 -0300

    using pointers for data mutation

--- pointers.go
+++ pointers.go
@@ -6,12 +6,13 @@ func main() {
 	age := 32           // regular variable
 	var agePointer *int // pointer
 	agePointer = &age
-	//fmt.Println("Age:", age)
+
 	fmt.Println("Age pointer:", *agePointer)
-	// adultYears := getAdultYears(age)
-	// fmt.Println("Adult Years:", adultYears)
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func editAgeToAdultYears(age *int) {
+	//return *age - 18
+	*age = *age - 18
 }
